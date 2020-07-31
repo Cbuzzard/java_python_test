a3c2707d90f2836a3dfd6fcaa8587e5d9d6b0fad
cbuzzard <corwin1998@gmail.com>
2020-07-30 21:26:58 -0400

    made small reddit script

--- src/python/main.py
+++ src/python/main.py
@@ -1,6 +1,21 @@
-f = open("test.txt", "a")
-f.write("Now the file has more content!")
-f.close()
+import praw
+import yaml
+import os
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, './config.yml')
 
-f = open("test.txt", "r")
-print(f.read())
+with open(filename) as file:
+    data = yaml.load(file, Loader=yaml.FullLoader)
+
+redditUser = data['reddit']['username']
+redditPass = data['reddit']['password']
+redditId = data['submission']['id']
+redditSecret = data['submission']['secret']
+
+reddit = praw.Reddit(client_id= redditId, client_secret= redditSecret, user_agent='my user agent', username= redditUser, password= redditPass)
+subreddit = reddit.subreddit('readitcheck')
+
+file = open("test.txt", "w")
+
+for submission in subreddit.top("all"):
+    file.write(submission.title + "\n")
