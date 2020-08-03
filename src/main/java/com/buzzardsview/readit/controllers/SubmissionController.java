80f62285c6855ed2ec4e30d7d56d049104ad67f7
cbuzzard <corwin1998@gmail.com>
2020-08-02 22:54:33 -0400

    added functionality to submission controller

--- src/main/java/com/buzzardsview/readit/controllers/SubmissionController.java
+++ src/main/java/com/buzzardsview/readit/controllers/SubmissionController.java
@@ -2,8 +2,7 @@ package com.buzzardsview.readit.controllers;
 
 import com.buzzardsview.readit.data.SubmissionRepository;
 import com.buzzardsview.readit.models.Submission;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +18,31 @@ public class SubmissionController {
     @GetMapping("/submissions")
     List<Submission> all() {
         return repository.findAll();
+    }
+
+    @PostMapping("/submissions")
+    Submission newSubmission(@RequestBody Submission submission) {
+        return repository.save(submission);
+    }
+
+    @GetMapping("/employees/{id}")
+    Submission one(@PathVariable Integer id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+    @PutMapping("/employees/{id}")
+    Submission replaceSubmission(@RequestBody Submission newSubmission, @PathVariable Integer id) {
+        return repository.findById(id).map(submission -> {
+            submission.setName(newSubmission.getName());
+            return repository.save(submission);
+        }).orElseGet(() -> {
+            newSubmission.setId(id);
+            return repository.save(newSubmission);
+        });
+    }
+
+    @DeleteMapping("/employees/{id}")
+    void deleteSubmission(@PathVariable Integer id) {
+        repository.deleteById(id);
     }
 }
